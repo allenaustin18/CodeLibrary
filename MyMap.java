a1fd8a1585ce5a62edae83fecffba6d73f457bb4
Allen Austin <44219576+allenaustin18@users.noreply.github.com>
2019-02-06 13:10:12 -0600

    Update MyMap.java

--- MyMap.java
+++ MyMap.java
@@ -173,3 +173,43 @@ public class MyMap<K, V>
 	
 
 }
+
+// Test file
+
+package com.test.coding;
+
+/**
+ * 
+ * @author Allen Austin - CS 320 - FA18 - Data Structures Programming Assignment
+ *         4 - Map that uses the BinarySearchTree The program implements a Map
+ *         data structure using a BinarySearchTree as its underlying data
+ *         structure
+ *
+ */
+/**
+ * Test Class contains the main method.
+	Main method tests all methods in MyMap class
+	mymap.put will populate BST
+ * 
+ *
+ */
+public class Test 
+{
+	public static void main(String args[]) 
+	{
+		
+		MyMap<String,Integer> myMap = new MyMap<>();
+		
+		 myMap.put("test1", 123);
+		
+		System.out.println(myMap.containsKey("test1"));
+		System.out.println(myMap.containsValue(123));
+		System.out.println(myMap.size());
+		System.out.println(myMap.toString());
+		System.out.println(myMap.get("test1"));
+		System.out.println(myMap.isEmpty());
+		System.out.println(myMap.remove("test1"));
+		
+	}
+}
+
